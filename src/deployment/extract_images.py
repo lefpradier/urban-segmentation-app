42ba2a7a0c8789910ab3a88e6014379efbd4189c
leapradier <lefpradier@gmail.com>
2023-10-10 17:05:54 +0200

    reshape images for app

---
+++ src/deployment/extract_images.py
@@ -0,0 +1,21 @@
+import os
+import cv2
+from glob import glob
+
+for f in glob(
+    "data/raw/P8_Cityscapes_leftImg8bit_trainvaltest/leftImg8bit/new_test/**/*.png",
+    recursive=True,
+):
+    image = cv2.imread(f)
+    image = cv2.resize(image, (256, 128))
+    path = "deployment/frontend/static/images/"
+    cv2.imwrite(path + os.path.basename(f), image)
+
+for f in glob(
+    "data/raw/P8_Cityscapes_gtFine_trainvaltest/gtFine/new_test/**/*labelIds.png",
+    recursive=True,
+):
+    image = cv2.imread(f)
+    image = cv2.resize(image, (256, 128))
+    path = "deployment/frontend/static/masks/"
+    cv2.imwrite(path + os.path.basename(f), image)
